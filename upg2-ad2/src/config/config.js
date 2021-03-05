8d959bf78c44dae74e253c7c08b02875837fbcbe
Oscar Andersson <oscar.andersson@soleil.se>
2021-03-05 11:11:45 +0100

    config done

--- upg2-ad2/src/config/config.js
+++ upg2-ad2/src/config/config.js
@@ -1,32 +1,36 @@
+/* eslint-disable func-names */
 /* eslint-disable no-undef */
-(function init() {
-  // Validator for email adresses
-  const validEmail = (email) => typeof email === 'string' && email.length > 0;
-  // Validator for numbers
-  const validNum = (num) => typeof num === 'number' && num > 0 && num < 100;
+(function () {
+  function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
   // overwrite default validate function
-  window.validate = function validate() {
-    const emailInput = document.querySelector('input[name=admin]');
-    const maxNumInput = document.querySelector('input[name=maxNum]');
+  window.validate = function validator() {
+    const emailInput = document.querySelector('#email');
+    const maxNumInput = document.querySelector('#number');
     let valid = true;
-    if (validEmail(emailInput)) {
+
+    if (!validateEmail(emailInput.value)) {
+      valid = false;
       this.$(emailInput)
         .parent()
         .addClass('has-error')
-        .append('<p class="error-message">Epostadress ej korrekt.</p>')
+        .append('<p class="error-message">Måste vara en epostadress.</p>')
         .closest('.panel-body')
         .addClass('highlight-flash');
-      valid = false;
     }
-    if (validNum(maxNumInput)) {
+
+    if (maxNumInput.value <= 0) {
+      valid = false;
       this.$(maxNumInput)
         .parent()
         .addClass('has-error')
-        .append('<p class="error-message">Vänligen mata in ett värde mellan 0 och 100.</p>')
+        .append('<p class="error-message">Måste vara ett heltak över noll.</p>')
         .closest('.panel-body')
         .addClass('highlight-flash');
-      valid = false;
     }
+
     return valid;
   };
 }());
