41c35615ae4643ebc4af410ab855577d156f1dc1
Oscar Andersson <oscar.andersson@soleil.se>
2021-03-29 04:38:09 +0200

    Config validation.

--- upg2-ad2/src/config/config.js
+++ upg2-ad2/src/config/config.js
@@ -1,6 +1,8 @@
 /* eslint-disable func-names */
 /* eslint-disable no-undef */
 (function () {
+  let showingEmail = false;
+  let showingNumber = false;
   function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
@@ -13,22 +15,28 @@
 
     if (!validateEmail(emailInput.value)) {
       valid = false;
-      this.$(emailInput)
-        .parent()
-        .addClass('has-error')
-        .append('<p class="error-message">Måste vara en epostadress.</p>')
-        .closest('.panel-body')
-        .addClass('highlight-flash');
+      if (!showingEmail) {
+        this.$(emailInput)
+          .parent()
+          .addClass('has-error')
+          .append('<p class="error-message">Måste vara en epostadress.</p>')
+          .closest('.panel-body')
+          .addClass('highlight-flash');
+        showingEmail = true;
+      }
     }
 
     if (maxNumInput.value <= 0) {
       valid = false;
-      this.$(maxNumInput)
-        .parent()
-        .addClass('has-error')
-        .append('<p class="error-message">Måste vara ett heltak över noll.</p>')
-        .closest('.panel-body')
-        .addClass('highlight-flash');
+      if (!showingNumber) {
+        this.$(maxNumInput)
+          .parent()
+          .addClass('has-error')
+          .append('<p class="error-message">Måste vara ett heltak över noll.</p>')
+          .closest('.panel-body')
+          .addClass('highlight-flash');
+        showingNumber = true;
+      }
     }
 
     return valid;
